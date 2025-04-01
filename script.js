0de42695ddb9e9ff717f78833f191f380db38561
Maharaj <60896049+ipractice108@users.noreply.github.com>
2025-04-01 11:27:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAYxYCjWEPB631FEIWLBN1E11Mm96Dwks0",
+  authDomain: "zamena-app.firebaseapp.com",
+  projectId: "zamena-app",
+  storageBucket: "zamena-app.firebasestorage.app",
+  messagingSenderId: "493406741561",
+  appId: "1:493406741561:web:df80dceb3ffe55378219e8",
+  measurementId: "G-VMFBSKLD83"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
